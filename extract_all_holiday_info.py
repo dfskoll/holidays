afbc0be6241fa77d511284219c819f96377e09cb
Dianne Skoll <dianne@skoll.ca>
2024-11-07 21:10:30 -0500

    Include long name of country and number of subdivisions.

--- extract_all_holiday_info.py
+++ extract_all_holiday_info.py
@@ -1,8 +1,16 @@
 import holidays
 from holidays import utils as hutils
 
+def get_long_country_name(code: str) -> str:
+    for k, v in holidays.registry.COUNTRIES.items():
+        if v[1] == code:
+            return v[0]
+    return code
+
 for (country, subdivisions) in hutils.list_supported_countries().items():
-    print("# COUNTRY %s" % (country))
+    longname = get_long_country_name(country)
+    subs = len(subdivisions)
+    print("# COUNTRY %s %d %s" % (country, subs, longname))
     x = holidays.country_holidays(country)
     x.get('2024-01-01')
     for subdiv in subdivisions:
